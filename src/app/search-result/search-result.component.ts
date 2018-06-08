e57ea4d66bc5dda541ee82ba7a641fba99690393
Manish Kumar Joshi <manish.joshi@irissoftware.com>
2018-06-08 12:50:20 +0530

    added animation in search result

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -2,11 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+  styleUrls: ['./search-result.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [ // each time the binding value changes
+        query(':leave', [
+          stagger(100, [
+            animate('0.5s', style({ opacity: 0 }))
+          ])
+        ], { optional: true }),
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(200, [
+            animate('0.5s', style({ opacity: 1 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class SearchResultComponent implements OnInit {
   searchList = Array<any>();
@@ -26,8 +44,14 @@ export class SearchResultComponent implements OnInit {
   }
 
   doSearch(searchTerm: string) {
-    this.db.list(`/locations/${this.location}/bsnl`).valueChanges().subscribe((data) => {
-      this.searchList = data;
+    const self = this;
+    this.db.list(`/locations/${this.location}`).valueChanges().subscribe((data) => {
+      data.forEach(function (res) {
+        const result = res as Array<any>;
+        result.forEach(function (gst) {
+          self.searchList.push(gst);
+        });
+      });
     });
   }
 
